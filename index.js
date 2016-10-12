635c4d2c4e07581b9ac237e1f0c3148bb37484ea
Amiel Perez <perezamiel@yahoo.com>
2016-10-12 13:52:21 +0800

    ES6ified

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ var postcss = require('postcss');
 var rater = require('./lib/specificity-rater')
 var _ = require('underscore')
 
-module.exports = postcss.plugin('postcss-specificity-ratings', function (opts) {
+module.exports = postcss.plugin('postcss-specificity-ratings', (opts) => {
     opts = opts || {};
     // Work with options here
-    return function (root, result) {
+    return (root, result) => {
       // Transform CSS AST here
-      root.walkRules(function(rule){
+      root.walkRules((rule) => {
         var ratings = rater.rate(rule.selector)
         var commentText = ""
-        _.each(ratings, function(rating){
-          commentText = commentText.concat("\n" + rating['selector'] + ": " + rating['specificity'])
-        })
-        rule.prepend(postcss.comment({ text: "Specificity: " + commentText }))
+        commentPhrases = _.map(ratings, (rating) => {
+                           return `${rating['selector']}: ${rating['specificity']}`
+                         })
+        rule.prepend(postcss.comment({ text: `Specificity: ${commentPhrases.join('; ')}` }))
       })
     };
 });
