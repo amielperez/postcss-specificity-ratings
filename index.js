720653c07d7a51289da695663a0e7abe1d07aeaa
Amiel Perez <perezamiel@yahoo.com>
2016-10-20 13:17:29 +0800

    Added feature to process `!important` declarations just to have an excuse to use `.walkDecls()`

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 var postcss = require('postcss');
-var rater = require('./lib/specificity-rater')
-var _ = require('underscore')
+var parser = require('./lib/parser')
 
 module.exports = postcss.plugin('postcss-specificity-ratings', (opts) => {
     opts = opts || {};
     // Work with options here
-    return (root, result) => {
-      // Transform CSS AST here
-      root.walkRules((rule) => {
-        var ratings = rater.rate(rule.selector)
-        var commentText = ""
-        commentPhrases = _.map(ratings, (rating) => {
-                           return `${rating['selector']}: ${rating['specificity']}`
-                         })
-        rule.prepend(postcss.comment({ text: `Specificity: ${commentPhrases.join('; ')}` }))
-      })
-    };
+    return parser;
 });
